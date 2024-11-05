fde9756793f25bfac719ada006dbeca24c5843e6
Vector247 <c.gillen@gmx.de>
2024-11-05 21:41:39 +0100

    Added main loop control

--- main.py
+++ main.py
@@ -6,15 +6,28 @@ import EMFIController
 import Plot
 
 # Configuration Parameter
-POSITION_CONTROLLER_PORT    = "COM25"   # Serial port connected to position controller
-EMFI_PROBE_PORT             = "COM23"   # Serial port connected to EMFI Probe
-EMFI_PROBE_BAUD             = 115200    # Baudrate of EMFI Probe
+POSITION_CONTROLLER_PORT = "COM25"  # Serial port connected to position controller
+EMFI_PROBE_PORT = "COM23"  # Serial port connected to EMFI Probe
+EMFI_PROBE_BAUD = 115200  # Baudrate of EMFI Probe
+
+
+class MainLoop:
+    run = False
+
+    def __init__(self):
+        self.run = True
+
+    def end_loop(self):
+        self.run = False
+
+    def is_running(self):
+        return self.run
 
 
 # Global Variables
+main_loop = MainLoop()
 pos_controller = PositionController.PositionController(POSITION_CONTROLLER_PORT)
 emfi_controller = EMFIController.EMFIController(EMFI_PROBE_PORT, EMFI_PROBE_BAUD)
-local_end = False
 
 
 # Functions
@@ -39,6 +52,7 @@ def print_menu():
     print("  X ->                 /_q_/    /_h_/       /_r_/           /_p_/        ")
     print("                                                                         ")
 
+
 def on_press(key):
     """
     Handles keyboard input
@@ -57,7 +71,9 @@ def on_press(key):
         pos_controller.move_rel(z=-1)
     if key == keyboard.KeyCode(char="r"):
         emfi_controller.arm()
-        results = pos_controller.moveRaster(width=2, height=1, action_callback=handle_pulse)
+        results = pos_controller.move_raster(
+            width=2, height=1, action_callback=handle_pulse
+        )
         emfi_controller.disarm()
         Plot.plot_results(results)
     if key == keyboard.KeyCode(char="h"):
@@ -69,7 +85,7 @@ def on_press(key):
     if key == keyboard.KeyCode(char="q"):
         pos_controller.close_controller()
         emfi_controller.close_controller()
-        local_end = True
+        main_loop.end_loop()
 
 
 def handle_pulse():
@@ -87,7 +103,7 @@ def main():
 
     print_menu()
 
-    while local_end != True:  # TODO: Find good way to end the script peacefully
+    while main_loop.is_running():
         pass
 
 
