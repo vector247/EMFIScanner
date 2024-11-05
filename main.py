0a064c512d4f7898814c8e5332255e4f202ea9d3
Christoph Gillen <c.gillen@gmx.de>
2024-11-05 09:12:04 +0100

    Added menu ascii art and some doc strings

--- main.py
+++ main.py
@@ -5,50 +5,88 @@ import PositionController
 import EMFIController
 import Plot
 
-posControl = PositionController.PositionController("COM25")
-emfiController = EMFIController.EMFIController("COM23", 115200)
+# Configuration Parameter
+POSITION_CONTROLLER_PORT    = "COM25"   # Serial port connected to position controller
+EMFI_PROBE_PORT             = "COM23"   # Serial port connected to EMFI Probe
+EMFI_PROBE_BAUD             = 115200    # Baudrate of EMFI Probe
+
+
+# Global Variables
+pos_controller = PositionController.PositionController(POSITION_CONTROLLER_PORT)
+emfi_controller = EMFIController.EMFIController(EMFI_PROBE_PORT, EMFI_PROBE_BAUD)
 local_end = False
 
 
+# Functions
+def print_menu():
+    """
+    Prints the keyboard menu
+    """
+    print("                                                                         ")
+    print("                               +--------------+                          ")
+    print("            |     |            | EMFI Scanner |                          ")
+    print("            |_____|            | by Vector247 |                          ")
+    print("              | |              +--------------+                          ")
+    print("               O                                                         ")
+    print("         ___________          Move XY         Move Z                     ")
+    print("        /          /          ____             ____                      ")
+    print("       /  ____    /          /_w_/            /_o_/                      ")
+    print("      /  /   /|  /     ____ ____ ____        ____                        ")
+    print("     /  r___/   /     /_a_//_s_//_d_/       /_l_/                        ")
+    print("^   /   |   |  / ^                                                       ")
+    print("|  /          / /        Quit   Home XY    Start Scan    Manual Pulse    ")
+    print("Z 0__________/ Y        ____     ____        ____            ____        ")
+    print("  X ->                 /_q_/    /_h_/       /_r_/           /_p_/        ")
+    print("                                                                         ")
+
 def on_press(key):
+    """
+    Handles keyboard input
+    """
     if key == keyboard.KeyCode(char="w"):
-        posControl.moveRel(y=1)
+        pos_controller.move_rel(y=1)
     if key == keyboard.KeyCode(char="s"):
-        posControl.moveRel(y=-1)
+        pos_controller.move_rel(y=-1)
     if key == keyboard.KeyCode(char="d"):
-        posControl.moveRel(x=1)
+        pos_controller.move_rel(x=1)
     if key == keyboard.KeyCode(char="a"):
-        posControl.moveRel(x=-1)
+        pos_controller.move_rel(x=-1)
     if key == keyboard.KeyCode(char="o"):
-        posControl.moveRel(z=1)
+        pos_controller.move_rel(z=1)
     if key == keyboard.KeyCode(char="l"):
-        posControl.moveRel(z=-1)
+        pos_controller.move_rel(z=-1)
     if key == keyboard.KeyCode(char="r"):
-        emfiController.arm()
-        results = posControl.moveRaster(width=2, height=1, action_callback=handle_pulse)
-        emfiController.disarm()
+        emfi_controller.arm()
+        results = pos_controller.moveRaster(width=2, height=1, action_callback=handle_pulse)
+        emfi_controller.disarm()
         Plot.plot_results(results)
     if key == keyboard.KeyCode(char="h"):
-        posControl.homeXY()
+        pos_controller.home_xy()
     if key == keyboard.KeyCode(char="p"):
-        emfiController.arm()
-        emfiController.pulse()
-        emfiController.disarm()
+        emfi_controller.arm()
+        emfi_controller.pulse()
+        emfi_controller.disarm()
     if key == keyboard.KeyCode(char="q"):
-        posControl.closeController()
-        emfiController.closeController()
+        pos_controller.close_controller()
+        emfi_controller.close_controller()
         local_end = True
 
 
 def handle_pulse():
+    """
+    Callback function handling the pulse generation
+    """
     time.sleep(1)
-    emfiController.pulse()
+    emfi_controller.pulse()
     # time.sleep(2)
 
 
 def main():
     listener = keyboard.Listener(on_press=on_press)
     listener.start()
+
+    print_menu()
+
     while local_end != True:  # TODO: Find good way to end the script peacefully
         pass
 
