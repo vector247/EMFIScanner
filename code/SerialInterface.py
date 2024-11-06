9e8dfe840720810b9b54f1aad4c3af457464ec97
Vector247 <c.gillen@gmx.de>
2024-11-06 23:38:04 +0100

    Added timout and read function to serial

--- code/SerialInterface.py
+++ code/SerialInterface.py
@@ -5,17 +5,27 @@ import time
 class SerialInterface:
     port = ""
 
-    def __init__(self, interface, baudrate=115200):
+    def __init__(self, interface, baudrate=115200, timeout=None):
         """
         Purpose: Opens a UART with baudrate 115200 on interface
         """
-        self.port = serial.Serial(port=interface, baudrate=baudrate)
+        self.port = serial.Serial(port=interface, baudrate=baudrate, timeout=timeout)
         print(f"Port opened on {self.port.name}")
 
     def output(self, output_string=""):
         print(f"Writing {output_string} on {self.port.name}")
         output_string += "\n\r"
         self.port.write(output_string.encode())
+
+    def input(self, n_bytes):
+        data = b""
+        self.port.reset_input_buffer()
+        data = self.port.read(n_bytes)
+        print(f"Read {data} from {self.port.name}")
+        return data
+
+    def check_open(self):
+        return self.port.is_open
 
     def closeInterface(self):
         print(f"Port {self.port.name} closed")
