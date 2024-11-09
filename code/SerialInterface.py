4b6e0fb1a6bf286f9520f8fe863f61a0cc7cc70c
Vector247 <c.gillen@gmx.de>
2024-11-09 16:16:22 +0100

    Removed print statements

--- code/SerialInterface.py
+++ code/SerialInterface.py
@@ -10,10 +10,10 @@ class SerialInterface:
         Purpose: Opens a UART with baudrate 115200 on interface
         """
         self.port = serial.Serial(port=interface, baudrate=baudrate, timeout=timeout)
-        print(f"Port opened on {self.port.name}")
+        # print(f"Port opened on {self.port.name}")
 
     def output(self, output_string=""):
-        print(f"Writing {output_string} on {self.port.name}")
+        # print(f"Writing {output_string} on {self.port.name}")
         output_string += "\n\r"
         self.port.write(output_string.encode())
 
@@ -23,21 +23,21 @@ class SerialInterface:
     def input(self, n_bytes):
         data = b""
         data = self.port.read(n_bytes)
-        print(f"Read {data} from {self.port.name}")
+        # print(f"Read {data} from {self.port.name}")
         return data
 
     def input_post_reset(self, n_bytes):
         data = b""
         self.port.reset_input_buffer()
         data = self.port.read(n_bytes)
-        print(f"Read {data} from {self.port.name}")
+        # print(f"Read {data} from {self.port.name}")
         return data
 
     def check_open(self):
         return self.port.is_open
 
     def closeInterface(self):
-        print(f"Port {self.port.name} closed")
+        # print(f"Port {self.port.name} closed")
         self.port.close()
 
 
